f4276ddd2cf5b15cb8cfab17beac3fa3b1678cd4
watchdog1023 <Naickercd@gmail.com>
2017-03-13 18:01:51 +0200

    Create TitanOS.cpp

---
+++ TitanOS.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<cstdio>
+//#include <dos.h>
+//for sleep fuction
+#include <conio.h> 
+//C libs to use system function
+#include<stdio.h>
+#include<stdlib.h>
+
+using namespace std;
+
+int main()
+{
+
+  return 0;
+}
