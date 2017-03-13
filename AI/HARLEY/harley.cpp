8fef21c6ce2f7d682d507707635157f6d7b1d9cb
watchdog1023 <Naickercd@gmail.com>
2017-03-13 17:58:17 +0200

    Create harley.cpp

---
+++ AI/HARLEY/harley.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<cstdio>
+//#include <dos.h>
+//for sleep fuction
+#include <conio.h> 
+//C libs to use system function
+#include<stdio.h>
+#include<stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hi,I am TitanOS AI Harley."<< endl;
+    cout << "I am starting the core functions of my program now."<<    endl;
+    cout << "Please wait"<<endl;
+    cout << "........" <<endl;
+    sleep(2);
+    cout << "........" <<endl;
+     sleep(2);
+    cout << "........" <<endl;
+     sleep(2);
+    cout << "........" <<endl;
+     sleep(2);
+    cout << "........" <<endl;
+     sleep(2);
+    cout << "........" <<endl;
+    sleep(2);
+    cout << "........" <<endl;
+     sleep(2);
+    cout << "........" <<endl;
+     sleep(2);
+    cout << "........" <<endl;
+     sleep(2);
+    cout << "I have started my core programing and am ready to help you." <<endl;
+    sleep(1);
+    system("clear");
+    cout <<"TitanOS Harley has Started"<<endl;
+    cout <<"what must I do for you?"<<endl;
+    string choice;
+    cout << "info" <<endl;
+    cout << "help" <<endl;
+    cout << "Protocol X" <<endl;
+    cin >>choice;
+    if(choice == "info")
+       {
+           cout << "hi";
+       }
+
+return 0;
+}
