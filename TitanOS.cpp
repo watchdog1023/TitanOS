f1f054f43e068dd4d54e2091da18ee26ccb5551d
watchdog1023 <Naickercd@gmail.com>
2017-03-17 18:43:04 +0200

    Add files via upload

--- TitanOS.cpp
+++ TitanOS.cpp
@@ -11,8 +11,98 @@
 
 using namespace std;
 
+void start();
+
 int main()
 {
+	start();
+}
 
-  return 0;
+void start()
+{
+	string ai_choice;
+	cout << "Which AI would you like to use?" << endl;
+	cout << "Alexa" << endl;
+	cout << "Betty" << endl;
+	cout << "Harley" << endl;
+	cout << "Lexa" << endl;
+	cout << "Sid" << endl;
+	cin >> ai_choice;	
+	
+	if(ai_choice == "alexa")
+		{
+			string final;
+			cout << "Are you sure?(This can't be changed without a reboot)" << endl;
+			cin >> final;
+			if(final == "y")
+				{
+					system("alexa");
+				}
+			if(final == "n")
+				{
+					system("clear");
+					start();
+				}
+		}
+	if(ai_choice == "betty")
+		{
+			string final;
+			cout << "Are you sure?(This can't be changed without a reboot)" << endl;
+			cin >> final;
+			if(final == "y")
+				{
+					system("betty");
+				}
+			if(final == "n")
+				{
+					system("clear");
+					start();
+				}
+		}
+	if(ai_choice == "harley")
+		{
+			string final;
+			cout << "Are you sure?(This can't be changed without a reboot)" << endl;
+			cin >> final;
+			if(final == "y")
+				{
+					system("harley");
+				}
+			if(final == "n")
+				{
+					system("clear");
+					start();
+				}
+		}
+	if(ai_choice == "lexa")
+		{
+			string final;
+			cout << "Are you sure?(This can't be changed without a reboot)" << endl;
+			cin >> final;
+			if(final == "y")
+				{
+					system("lexa");
+				}
+			if(final == "n")
+				{
+					system("clear");
+					start();
+				}
+		}
+	if(ai_choice == "sid")
+		{
+			string final;
+			cout << "Are you sure?(This can't be changed without a reboot)" << endl;
+			cin >> final;
+			if(final == "y")
+				{
+					system("sid");
+				}
+			if(final == "n")
+				{
+					system("clear");
+					start();
+				}
+		}
+	
 }
